29e440f2dcf46a365d3d01c7bb6aff2b795efabb
LordOfRaptor <quentin.noir@gmail.com>
2021-11-26 22:39:18 +0100

    completion de la classe entity Account

--- revolut/src/main/java/fr/miage/revolut/entities/Account.java
+++ revolut/src/main/java/fr/miage/revolut/entities/Account.java
@@ -3,8 +3,7 @@ package fr.miage.revolut.entities;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -13,6 +12,28 @@ public class Account{
 
     @Id
     private String uuid;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "passport", length = 9)
+    private String passport;
+
+    @Column(name = "phone_number")
+    //@Pattern(regexp = "^\\+[1-9]{1,14}$")
+    private String phoneNumber;
+
+    @Column(name = "iban")
+    private String iban;
+
+//@Column(name = "token")
+    //private String token;
 
 
 
