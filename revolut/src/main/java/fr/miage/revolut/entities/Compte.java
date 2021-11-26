130199d9c0f4e14b1338e9735a1102fe9f8abb46
LordOfRaptor <quentin.noir@gmail.com>
2021-11-26 21:45:52 +0100

    first commit

---
+++ revolut/src/main/java/fr/miage/revolut/entities/Compte.java
@@ -0,0 +1,18 @@
+package fr.miage.revolut.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Compte{
+
+    @Id
+    private String uuid;
+
+
+}
---
+++ revolut/src/test/java/fr/miage/revolut/RevolutApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.revolut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevolutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
