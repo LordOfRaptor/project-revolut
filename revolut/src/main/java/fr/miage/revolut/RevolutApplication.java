2b4cb6e87848e70d2e139fc3b3c57af5803b44cc
LordOfRaptor <quentin.noir@gmail.com>
2021-12-11 17:34:20 +0100

    Ajout d'open API pour de la documentation

--- revolut/src/main/java/fr/miage/revolut/RevolutApplication.java
+++ revolut/src/main/java/fr/miage/revolut/RevolutApplication.java
@@ -1,13 +1,24 @@
 package fr.miage.revolut;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RevolutApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RevolutApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI intervenantAPI() {
+		return new OpenAPI().info(new Info()
+				.title("revolut API")
+				.version("1.0")
+				.description("Documentation sommaire de l'API revolut 1.0"));
 	}
 
 }
