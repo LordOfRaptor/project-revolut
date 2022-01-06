dc25fb4a10eee7f3db30741d3653e7e4cbbb30de
LordOfRaptor <quentin.noir@gmail.com>
2022-01-06 15:53:30 +0100

    Ajout d'un exception handler

---
+++ revolut/src/main/java/fr/miage/revolut/exceptionsHandler/AccountsControllerAdvice.java
@@ -0,0 +1,38 @@
+package fr.miage.revolut.exceptionsHandler;
+
+import fr.miage.revolut.controllers.AccountsController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ResponseBody
+@ControllerAdvice(basePackageClasses = AccountsController.class)
+public class AccountsControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex, WebRequest request,HttpServletRequest httpRequest) {
+        Map<String, String> json = new LinkedHashMap<>();
+        json.put("status",String.valueOf(HttpStatus.BAD_REQUEST.value()));
+        ex.getBindingResult().getAllErrors().forEach( error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            json.put(fieldName, errorMessage);
+        });
+        json.put("path",httpRequest.getRequestURI());
+
+
+
+
+        return json;
+    }
+}
